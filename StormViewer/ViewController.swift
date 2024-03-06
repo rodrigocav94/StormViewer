90ae767aed4091970f0cc331de517d6f1eb62ece
Rodrigo A. Cavalcanti <rodrigocav94@gmail.com>
2024-03-06 04:31:23 -0300

    Listing images with FileManager

--- StormViewer/ViewController.swift
+++ StormViewer/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var pictures = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        populatePictures()
     }
 
-
+    func populatePictures() {
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath! // iOS projects always habe a resource path.
+        let items = try! fm.contentsOfDirectory(atPath: path) // Impossible to run any project without loading the resource content.
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                // This is a picture to load!
+                pictures.append(item)
+            }
+        }
+    }
 }
 
+#Preview {
+  let button = ViewController()
+  return button
+}
