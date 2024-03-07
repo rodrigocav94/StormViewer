43e9ec27bc6932cae4ca13aa985bbc10990b4be3
Rodrigo A. Cavalcanti <rodrigocav94@gmail.com>
2024-03-06 21:20:10 -0300

    Added navigation to detail view controller

--- StormViewer/DetailViewController.swift
+++ StormViewer/DetailViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class DetailViewController: UIViewController {
     @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        loadImage()
+    }
+    
+    func loadImage() {
+        if let selectedImage {
+            imageView.image = UIImage(named: selectedImage)
+        }
     }
     
 
